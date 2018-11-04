b266b1399a3f8d8753304d583d3804b9cd5ad079
praveen970 <praveeneyeruva970@gmail.com>
2018-11-03 23:45:39 -0700

    Commit with box Auth and log 3

--- routes/boxAuth.js
+++ routes/boxAuth.js
@@ -31,41 +31,7 @@ router.get('/', function(req, res) {
     res.redirect(authEndpoint);
 });
 
-router.get('/return', function(req, res) {
-    // Extract auth code and state
-    const state = req.query.state;
-    const code = req.query.code;
 
-    // Exchange auth code for an access token
-    sdk.getTokensAuthorizationCodeGrant(code, null, function(err, tokenInfo) {
-        if (err) {
-            console.error(err);
-        }
-
-        // Create new token store instance, and write to it
-        var tokenStore = new TokenStore();
-        tokenStore.write(tokenInfo, function(storeErr) {
-            if (err) {
-                console.error(err);
-            }
-
-            // Create new persistent client with token storage
-            var client = sdk.getPersistentClient(tokenInfo, tokenStore);
-
-            // Get current user information and display
-            client.users.get(client.CURRENT_USER_ID, null, function(err, currentUser) {
-                if(err) throw err;
-                console.log(currentUser);
-            });
-        });
-
-        /*const client = sdk.getBasicClient(tokenInfo.accessToken);
-        client.users.get(client.CURRENT_USER_ID, null, function(err, currentUser) {
-          if(err) throw err;
-          console.log('Hello, ' + currentUser.name + '!');
-        });*/
-    });
-});
 
 // Create Express server
 
--- routes/boxAuthReturn.js
+++ routes/boxAuthReturn.js
@@ -2,10 +2,40 @@ var express = require('express');
 var router = express.Router();
 
 
-router.get('/', function (req, res, next) {
-    var code = req.query.code;
-    console.log(code);
-    res.send(code);
-})
+router.get('/', function(req, res) {
+    // Extract auth code and state
+    const state = req.query.state;
+    const code = req.query.code;
+
+    // Exchange auth code for an access token
+    sdk.getTokensAuthorizationCodeGrant(code, null, function(err, tokenInfo) {
+        if (err) {
+            console.error(err);
+        }
+
+        // Create new token store instance, and write to it
+        var tokenStore = new TokenStore();
+        tokenStore.write(tokenInfo, function(storeErr) {
+            if (err) {
+                console.error(err);
+            }
+
+            // Create new persistent client with token storage
+            var client = sdk.getPersistentClient(tokenInfo, tokenStore);
+
+            // Get current user information and display
+            client.users.get(client.CURRENT_USER_ID, null, function(err, currentUser) {
+                if(err) throw err;
+                res.send(currentUser);
+            });
+        });
+
+        const client = sdk.getBasicClient(tokenInfo.accessToken);
+        client.users.get(client.CURRENT_USER_ID, null, function(err, currentUser) {
+          if(err) throw err;
+          res.send('Hello, ' + currentUser.name + '!');
+        });
+    });
+});
 
 module.exports = router;
