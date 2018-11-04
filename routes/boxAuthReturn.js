cd21021d805c9effa727f6e71af676a8d9eede62
praveen970 <praveeneyeruva970@gmail.com>
2018-11-03 23:42:11 -0700

    Commit with box Auth and log 2

--- routes/boxAuthReturn.js
+++ routes/boxAuthReturn.js
@@ -5,6 +5,7 @@ var router = express.Router();
 router.get('/', function (req, res, next) {
     var code = req.query.code;
     console.log(code);
+    res.send(code);
 })
 
 module.exports = router;
