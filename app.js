ae6ed08131f5f8577f1c33a88e2f37037cb4e44c
praveen970 <praveeneyeruva970@gmail.com>
2018-11-03 23:00:09 -0700

    Clarifai commit

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+var imageRouter = require('./routes/image_upload');
 var app = express();
 
 app.use(logger('dev'));
@@ -16,5 +16,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+app.use('/image_upload',imageRouter);
 module.exports = app;
---
+++ routes/image_upload.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+const Clarifai = require('clarifai');
+var fs = require('fs');
+
+const app = new Clarifai.App({
+    apiKey: '5fdd916715f34b26ab5345b1e709c5cd'
+});
+
+router.get('/', function (req, res, next) {
+    var code = req.query.code;
+    console.log(code);
+    var imgStr= fs.readFileSync('./public/images/heart.jpg','base64');
+    app.models.predict(Clarifai.GENERAL_MODEL, imgStr).then(
+        function(response) {
+            var jsonData = JSON.parse(JSON.stringify(response));
+            var conceptsArray = jsonData.outputs[0].data.concepts;
+            var tagArray = [];
+            for(var i=0;i<conceptsArray.length;i++){
+                tagArray.push(conceptsArray[i].name);
+            }
+            res.send(tagArray)
+
+
+        },
+        function(err) {
+            console.error(err);
+        }
+    );
+})
+
+
+
+module.exports = router;
